d77f13bdca6cef4788a1e3272c375635d9445435
Oleksii Stepanenko <stepanenko.oleksii@gmail.com>
2020-11-01 13:13:45 +0100

    task 03-01-limitSize

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,14 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = +options.limit || Number.MAX_SAFE_INTEGER;
+    this.size = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.size += chunk.length;
+    let error = (this.size > this.limit) ? new LimitExceededError() : null;
+    callback(error, chunk);
   }
 }
 
