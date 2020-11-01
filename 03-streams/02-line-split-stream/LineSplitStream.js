f48ef6a5f9d13c7e7f1d7ebfc8465c914fc9caac
Oleksii Stepanenko <stepanenko.oleksii@gmail.com>
2020-11-01 16:24:36 +0100

    task 03-02-lineSplit

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -3,13 +3,26 @@ const os = require('os');
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
+    options.decodeStrings = false;
     super(options);
+
+    this.trail = "";
   }
 
   _transform(chunk, encoding, callback) {
+    var lines = (this.trail+chunk).split(os.EOL);
+    this.trail = lines.pop();
+    lines.forEach(this.push.bind(this));
+    callback();
   }
 
   _flush(callback) {
+    console.log('flush');
+    if (this.trail.length > 0) {
+      this.push(this.trail);
+      this.trail = "";
+    }
+    callback();
   }
 }
 
