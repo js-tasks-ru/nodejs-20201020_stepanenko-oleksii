ae6b18ce8f92f24ba977a11ee0fa25ed48da983a
Oleksii Stepanenko <stepanenko.oleksii@gmail.com>
2020-11-04 09:30:14 +0100

    task 06-koa methods {subscribe, publish} implemented

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -7,11 +7,24 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+let subscribers = [];
 
 router.get('/subscribe', async (ctx, next) => {
+    await new Promise((resolve, reject) => {
+        subscribers.push(resolve);
+    }).then ((res) => {ctx.body=res;} );
 });
 
 router.post('/publish', async (ctx, next) => {
+    if (!ctx.request.body.message) {
+        return next();
+    }
+    subscribers.forEach(
+        resolve => resolve(ctx.request.body.message)
+    )
+    subscribers = [];
+    ctx.status = 201;
+    ctx.body="OK";
 });
 
 app.use(router.routes());
