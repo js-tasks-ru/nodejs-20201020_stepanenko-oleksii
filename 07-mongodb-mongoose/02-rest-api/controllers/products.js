6d398035c79c0f6a7927293524f28bd26e0ce3c7
Oleksii Stepanenko <stepanenko.oleksii@gmail.com>
2020-11-08 11:58:23 +0100

    task 07-02

--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,12 +1,25 @@
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  if (!ctx.request.query.subcategory) {
+    return next();
+  }
+  ctx.body = {products: await Product.find({'subcategory':ctx.request.query.subcategory})};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  ctx.body = {products: await Product.find()};
+  next();
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  if (!mongoose.Types.ObjectId.isValid (ctx.params.id))
+    ctx.throw(400, `Id:${ctx.params.id} is not valid`);
+
+  var product = await Product.findOne({'_id': ctx.params.id});
+  if (!product)
+    ctx.throw(404, `No product with id:${ctx.params.id}`);
+
+  ctx.body = {product: product};
 };
 
